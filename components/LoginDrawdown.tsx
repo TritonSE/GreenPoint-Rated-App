7a35604185d1eb4db5e93f50bf2ec69021084863
skaiwu <57241710+skaiwu@users.noreply.github.com>
2020-05-19 00:06:47 -0700

    sign in and login page stylesheet adds

--- components/LoginDrawdown.tsx
+++ components/LoginDrawdown.tsx
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { StyleSheet } from "react-native";
 import { Dropdown } from "react-native-material-dropdown";
 
 var DATA = [
@@ -11,5 +12,23 @@ var DATA = [
 ];
 
 export default function LoginDrawdown(props) {
-  return <Dropdown label={props.type} data={DATA} />;
+  return <Dropdown dropdownOffset={{top:5}} containerStyle={styles.menu}
+          rippleCentered={true}
+          fontSize={14}
+          inputContainerStyle={{ borderBottomColor: 'transparent' }}
+          valueExtractor={({value})=> value} label={props.type} data={DATA} />;
 }
+
+const styles = StyleSheet.create({
+    menu: {
+        borderWidth:1,
+        backgroundColor: 'white',
+        borderColor:'#878787',
+        width: '90%',
+        height: '13.5%',
+        paddingLeft: '4%',
+        borderWidth: 1.5,
+        borderRadius: 10,
+        marginTop: '4%',
+        },
+});
